932aba75ec9c267f10c1456c7b32bacc1888a8d7
jsantist3ban <jsantisteban@n2x.io>
2025-02-06 14:10:16 +0100

    fix: error service

--- scripts/windows-install.ps1
+++ scripts/windows-install.ps1
@@ -205,7 +205,7 @@ Set-Service -Name $Constants.ServiceName -StartupType Automatic
 
 # Modify the service to depend on the "Network" service
 $serviceRegistryPath = "HKLM:\SYSTEM\CurrentControlSet\Services\$($Constants.ServiceName)"
-$networkDependency = "Netlogon"
+$networkDependency = "Tcpip"
 Set-ItemProperty -Path $serviceRegistryPath -Name DependOnService -Value $networkDependency
 
 Write-Log -LogLevel "info" -Message "$($Constants.ServiceName) is now configured to start after the $networkDependency service."
