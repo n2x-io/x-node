d6979701cc06544ed0039957912c4ead3539aa03
jsantist3ban <jsantisteban@n2x.io>
2025-02-06 14:13:52 +0100

    fix: remove last config

--- scripts/windows-install.ps1
+++ scripts/windows-install.ps1
@@ -203,13 +203,6 @@ if (-not $service) {
 # Ensure the service is set to start automatically at boot
 Set-Service -Name $Constants.ServiceName -StartupType Automatic
 
-# Modify the service to depend on the "Network" service
-$serviceRegistryPath = "HKLM:\SYSTEM\CurrentControlSet\Services\$($Constants.ServiceName)"
-$networkDependency = "Tcpip"
-Set-ItemProperty -Path $serviceRegistryPath -Name DependOnService -Value $networkDependency
-
-Write-Log -LogLevel "info" -Message "$($Constants.ServiceName) is now configured to start after the $networkDependency service."
-
 # Check if the service is running
 if ($service -and $service.Status -eq 'Running') {
     Write-Log -LogLevel "info" -Message "$($Constants.ServiceName) service is already running."
