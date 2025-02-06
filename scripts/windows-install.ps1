d017bcb63cdbb509bb239c4d46be061fe904cf14
jsantist3ban <jsantisteban@n2x.io>
2025-02-06 12:53:28 +0100

    fix: remove scheduledtask

--- scripts/windows-install.ps1
+++ scripts/windows-install.ps1
@@ -208,28 +208,31 @@ if ($service -and $service.Status -eq 'Running') {
     Start-Service $Constants.ServiceName
 }
 
+# Ensure the service is set to start automatically at boot
+Ensure-ServiceAutoStart -serviceName $Constants.ServiceName
+
 # Check if the scheduled task already exists
-$taskName = "n2xNodeStartupTask"
-$existingTask = Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue
-
-if (-not $existingTask) {
-    Write-Log -LogLevel "info" -Message "Scheduled task $taskName does not exist. Creating it."
-
-   # Define the scheduled task action
-    $action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-File $n2xNodeBinaryPath"
-
-    # Define the trigger to run at system startup
-    $trigger = New-ScheduledTaskTrigger -AtStartup
-
-    # Define task settings for reliability
-    $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -RunOnlyIfNetworkAvailable -RestartCount 3 -RestartInterval (New-TimeSpan -Minutes 1)
-
-    # Register the task
-    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Description "Runs n2x-node at startup" -Settings $settings -User "SYSTEM" -RunLevel Highest
-
-    Write-Log -LogLevel "info" -Message "Scheduled task $taskName created successfully."
-} else {
-    Write-Log -LogLevel "info" -Message "Scheduled task $taskName already exists. Skipping creation."
-}
+# $taskName = "n2xNodeStartupTask"
+# $existingTask = Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue
+# 
+# if (-not $existingTask) {
+#     Write-Log -LogLevel "info" -Message "Scheduled task $taskName does not exist. Creating it."
+# 
+#    # Define the scheduled task action
+#     $action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-File $n2xNodeBinaryPath"
+# 
+#     # Define the trigger to run at system startup
+#     $trigger = New-ScheduledTaskTrigger -AtStartup
+# 
+#     # Define task settings for reliability
+#     $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -RunOnlyIfNetworkAvailable -RestartCount 3 -RestartInterval (New-TimeSpan -Minutes 1)
+# 
+#     # Register the task
+#     Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Description "Runs n2x-node at startup" -Settings $settings -User "SYSTEM" -RunLevel Highest
+# 
+#     Write-Log -LogLevel "info" -Message "Scheduled task $taskName created successfully."
+# } else {
+#     Write-Log -LogLevel "info" -Message "Scheduled task $taskName already exists. Skipping creation."
+# }
 
 Write-Log -LogLevel "info" -Message "Script completed successfully."
